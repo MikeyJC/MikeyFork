b53e9b105eba25da13be23480d64f63f8d21d918
michael <michael.crews@salesagility.com>
2017-10-31 12:56:41 +0000

    WIP: Added 'Insert into PDF' button to quotes, will add pdf template into predefined pdf

---
+++ custom/Extension/application/Ext/EntryPointRegistry/CustomPdfEntryPoint.php
@@ -0,0 +1,5 @@
+<?php
+$entry_point_registry['customGeneratePdf'] = array(
+    'file' => 'custom/modules/AOS_PDF_Templates/customGeneratePdf.php',
+    'auth' => false
+);
---
+++ custom/Extension/modules/AOS_Quotes/Ext/Language/en_us.custom_pdf.php
@@ -0,0 +1,2 @@
+<?php
+$mod_strings['LBL_INSERT_INTO_PDF'] = 'Insert into PDF';
